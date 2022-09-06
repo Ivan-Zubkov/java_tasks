21abf9396fcde64ff1b49204bbf3ae58493bc841
Ivan-Zubkov <ivanzubkov200476@gmail.com>
2022-09-06 17:35:38 +0300

    "new changes"

--- pract_2/TestBall.java
+++ pract_2/TestBall.java
@@ -2,9 +2,8 @@ package pract_2;
 
 public class TestBall {
     public static void main(String[] args) {
-        //Ball b1 = new Ball();
-        //System.out.println(b1);
-        //delete b1;
+        Ball b2 = new Ball();
+        System.out.println(b2);
 
         Ball b1 = new Ball(100, 100);
         System.out.println(b1);
@@ -14,7 +13,7 @@ public class TestBall {
 
         b1.setX(20);
 
-        System.out.println(b1.getX(20););
+        System.out.println(b1.getX());
 
         b1.setXY(12,82);
         System.out.println(b1);
